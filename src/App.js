2692543160c6f54f249f36b873a62d6236268bb8
mohammedaboohamid <mohammedaboohamidkp@gmail.com>
2024-09-10 16:33:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React,{useState} from "react";
 function App() {
+const [count,setCount]=useState(0)
+
+/*
+  const increment=()=>{
+    setCount(count+1)
+
+  }
+    */
+
+  const decrement=()=>{
+    setCount(count-1)
+  }
+  const reset =()=>{
+    setCount(0)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col justify-center items-center h-[500px]">
+      <h1 className="text-3xl font-bold text-8xl bg-green-200">{count}</h1>
+     <div className="flex items-center gap-2 ">
+     <button onClick={()=>{count===0 ?setCount(count):setCount(count-1)}} className="font-bold border border-black text-red-600 bg-slate-400 text-4xl px-1  bg-slate-400">-</button>
+
+     <button onClick={reset} className="font-bold border border-black text-4xl bg-slate-400">reset</button>
+
+     <button onClick={()=>{count===10 ?setCount(count):setCount(count+1)}} className="font-bold border border-black text-green-600 bg-slate-400 text-4xl  bg-slate-400">+</button>
+    
+         
+     
+     </div>
     </div>
   );
 }
